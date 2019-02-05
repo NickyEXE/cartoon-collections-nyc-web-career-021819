fdc7a32829d0d528ee4ea9c9473af760c60be33b
Nicholas Dover <dovern42@gmail.com>
2019-02-05 06:37:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |element|
-    cheese_types.include(element)
+    cheese_types.include?(element)
   end
 end
 
